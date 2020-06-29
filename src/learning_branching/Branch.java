cdff802985a7900df75737b8a6b71b838a91ce23
Jeel Salvi <jeel12393@gmail.com>
2020-06-29 12:19:27 -0400

    Second Round

--- src/learning_branching/Branch.java
+++ src/learning_branching/Branch.java
@@ -1,12 +1,11 @@
 package learning_branching;
 
-public class Tutorial {
+public class Branch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("This is programmer");
 		System.out.print("This is my First Project");
-		System.out.print("This is my First Project");
+		System.out.print("cool");
 	}
 
 }
